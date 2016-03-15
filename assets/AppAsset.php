917e8eb67aa548652a770aad965815c2b51650e3
jerico1990 <cesar.gago.egocheaga@gmail.com>
2016-03-15 12:34:23 -0500

    prueba2

---
+++ assets/AppAsset.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class AppAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        //'css/site.css',
+        //'css/default.css',
+        'css/index.css',
+        'css/jquery.fullPage.css'
+        //'http://fonts.googleapis.com/css?family=Headland+One|Open+Sans:400italic,400,700',
+    ];
+    public $js = [
+        
+        //'js/angular.js',
+        //'http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js',
+        //'js/app.js',
+        'js/jquery.fullPage.js',
+        'js/jquery.slimscroll.min.js',
+        'js/jquery.hovercard.js',
+        'js/bootstrap-notify.js',
+        'js/extensions.js',
+        '//code.jquery.com/ui/1.11.4/jquery-ui.js',
+        'js/typeahead.bundle.js'
+        //'js/notify.js',
+        //'js/notify.min.js'
+        //'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
+        //'http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js'
+        //'js/jquery-1.10.2.min.js',
+        //'js/scrollIt.js',
+        //'https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js'
+        //'https://code.angularjs.org/2.0.0-beta.0/angular2-polyfills.js',
+        //'https://code.angularjs.org/2.0.0-beta.0/Rx.umd.js',
+        //'https://code.angularjs.org/2.0.0-beta.0/angular2-all.umd.dev.js'
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+}
